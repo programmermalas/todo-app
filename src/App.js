f7f316caa3c08ea11f9a43ae100a6a04c8adc50f
irfan hadian <irfan.hadian10@gmail.com>
2020-07-13 15:27:00 +0700

    app js now is using hooks

--- src/App.js
+++ src/App.js
@@ -1,123 +1,123 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashAlt, faEdit } from "@fortawesome/free-solid-svg-icons";
 
 import * as actions from "./redux/actions";
 
-class App extends Component {
-  state = {
+const App = () => {
+  const [state, setState] = useState({
     todo: "",
     id: -1,
-  };
+  });
 
-  inputHandler = (event) => {
+  const todo = useSelector((state) => state.todo);
+
+  const dispatch = useDispatch();
+
+  const inputHandler = (event) => {
     const { name, value } = event.target;
 
-    this.setState({ [name]: value });
+    setState({ ...state, [name]: value });
   };
 
-  submitHandler = (event) => {
+  const submitHandler = (event) => {
     event.preventDefault();
 
-    if (this.state.id >= 0) {
-      this.props.onUpdateTodo(this.state.id, this.state.todo);
+    if (state.id >= 0) {
+      dispatch(actions.update_todo(state.id, state.todo));
     } else {
-      this.props.onAddTodo(this.state.todo);
+      dispatch(actions.add_todo(state.todo));
     }
 
-    this.setState({ todo: "", id: -1 });
+    setState({ todo: "", id: -1 });
   };
 
-  editHandler = (index, todo) => {
-    this.setState({ id: index, todo: todo });
+  const editHandler = (id, todo) => {
+    setState({ id: id, todo: todo });
   };
 
-  deleteHandler = (todo) => {
-    this.props.onDeleteTodo(todo);
+  const cancelHandler = () => {
+    setState({ id: -1, todo: "" });
   };
 
-  render() {
-    const { todo } = this.props;
+  const deleteHandler = (id) => {
+    dispatch(actions.delete_todo(id));
+  };
 
-    return (
-      <div className="h-screen flex flex-col justify-center items-center bg-purple-100">
-        <div className="w-104 shadow-lg bg-purple-400 p-3 mb-5">
-          <form id="form-todo" onSubmit={this.submitHandler}>
-            <input
-              name="todo"
-              type="text"
-              className="w-full text-xl text-white text-center font-semibold placeholder-white  bg-transparent outline-none"
-              placeholder="Todo Here..."
-              value={this.state.todo}
-              onChange={this.inputHandler}
-            />
-          </form>
-        </div>
+  const lists = todo.todos.map((element) => (
+    <li key={element.id} className="flex justify-between mb-4">
+      {element.todo}
+      <div>
+        <button
+          className="mr-2 hover:text-purple-400"
+          onClick={() => editHandler(element.id, element.todo)}
+        >
+          <FontAwesomeIcon icon={faEdit} />
+        </button>
 
-        <div className="flex flex-col items-center">
-          <div className="w-104 shadow-lg bg-white p-10">
-            <ul className="font-semibold text-lg text-gray-700">
-              {todo.todos.map((element, index) => (
-                <li key={index} className="flex justify-between mb-4">
-                  {element.todo}
-                  <div>
-                    <button
-                      className="mr-2 hover:text-purple-400"
-                      onClick={() => this.editHandler(index, element.todo)}
-                    >
-                      <FontAwesomeIcon icon={faEdit} />
-                    </button>
-
-                    <button
-                      className="hover:text-purple-400"
-                      onClick={() => this.deleteHandler(element.todo)}
-                    >
-                      <FontAwesomeIcon icon={faTrashAlt} />
-                    </button>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-
-          <div className="-mt-6">
-            {this.state.id >= 0 ? (
-              <button
-                type="submit"
-                className="text-xl text-white font-semibold rounded-full shadow-lg bg-purple-400 py-3 px-10 hover:bg-purple-500"
-                form="form-todo"
-              >
-                Edit
-              </button>
-            ) : (
-              <button
-                type="submit"
-                className="text-xl text-white font-semibold rounded-full shadow-lg bg-purple-400 py-3 px-10 hover:bg-purple-500"
-                form="form-todo"
-              >
-                Save
-              </button>
-            )}
-          </div>
-        </div>
+        <button
+          className="hover:text-purple-400"
+          onClick={() => deleteHandler(element.id)}
+        >
+          <FontAwesomeIcon icon={faTrashAlt} />
+        </button>
       </div>
+    </li>
+  ));
+
+  const button =
+    state.id >= 0 ? (
+      <React.Fragment>
+        <button
+          type="submit"
+          className="text-xl text-white font-semibold rounded-full shadow-lg bg-purple-400 py-3 px-10 mr-2 hover:bg-purple-500"
+          form="form-todo"
+        >
+          Edit
+        </button>
+
+        <button
+          className="text-xl text-white font-semibold rounded-full shadow-lg bg-purple-400 py-3 px-10 hover:bg-purple-500"
+          onClick={cancelHandler}
+        >
+          Cancel
+        </button>
+      </React.Fragment>
+    ) : (
+      <button
+        type="submit"
+        className="text-xl text-white font-semibold rounded-full shadow-lg bg-purple-400 py-3 px-10 hover:bg-purple-500"
+        form="form-todo"
+      >
+        Save
+      </button>
     );
-  }
-}
 
-const mapStateToProps = (state) => {
-  return {
-    todo: state.todo,
-  };
+  return (
+    <div className="h-screen flex flex-col justify-center items-center bg-purple-100">
+      <div className="w-104 shadow-lg bg-purple-400 p-3 mb-5">
+        <form id="form-todo" onSubmit={submitHandler}>
+          <input
+            name="todo"
+            type="text"
+            className="w-full text-xl text-white text-center font-semibold placeholder-white  bg-transparent outline-none"
+            placeholder="Todo Here..."
+            value={state.todo}
+            onChange={inputHandler}
+          />
+        </form>
+      </div>
+
+      <div className="flex flex-col items-center">
+        <div className="w-104 shadow-lg bg-white p-10">
+          <ul className="font-semibold text-lg text-gray-700">{lists}</ul>
+        </div>
+
+        <div className="-mt-6">{button}</div>
+      </div>
+    </div>
+  );
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onAddTodo: (todo) => dispatch(actions.add_todo(todo)),
-    onUpdateTodo: (todo, index) => dispatch(actions.update_todo(todo, index)),
-    onDeleteTodo: (index) => dispatch(actions.delete_todo(index)),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
